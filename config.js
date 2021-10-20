99757544df6a1164bce2cd74e697ec12c87c5903
duc2003bui <92872180+duc2003bui@users.noreply.github.com>
2021-10-20 21:44:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Em';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Thời gian có thể trôi đi, mọi thứ có thể thay đổi nhưng tình yêu anh dành cho em sẽ mãi không bao giờ phai nhạt. Em thấy khó tin lắm đúng không?. Vậy thì hãy để anh chứng minh cho em thấy nhé người anh yêu. Cho dù thế nào thì cũng hãy nắm lấy tay nhau nhé. Mãi yêu em từ giờ cho đến mãi về sau <3';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
